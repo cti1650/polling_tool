d32f8f34b040a52cfe738e7d136579279cd2e53b
cti1650 <cti1650@gmail.com>
2023-01-21 00:18:20 +0900

    feat: update config sample

--- src/config_sample.js
+++ src/config_sample.js
@@ -1,3 +1,7 @@
 module.exports = {
-  polling_list: [{ address: "google.com", port: 443, mode: "tcp" }],
+  polling_list: [
+    { address: "google.com", port: 443, mode: "tcp" },
+    { address: "localhost", port: 12345, mode: "udp" },
+    { address: "127.0.0.1", port: 12345, mode: "udp" },
+  ],
 };
